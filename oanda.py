1daf16de920e79e5f07167f6e8eb24f15fde3a46
mniyk <my.name.is.yohei.kono@gmail.com>
2022-02-10 20:57:09 +0900

    Add: create oanda.py initializer

--- oanda.py
+++ oanda.py
@@ -1,2 +1,21 @@
 """OANDA APIを操作するためのモジュール
 """
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Oanda:
+    def __init__(self, account_id: str, access_token: str) -> None:
+        """初期化
+
+        Args:
+            account_id (str): アカウントID
+            access_token (str): アクセストークン
+
+        Examples:
+            >>> api = Oanda(account_id='test_id', access_token='test_token')
+        """
+        self.account_id = account_id
+        self.access_token = access_token
--- tests/test_oanda.py
+++ tests/test_oanda.py
@@ -1,2 +1,32 @@
 """oanda.pyのunittest
 """
+import configparser
+import logging
+import unittest
+
+from oanda import Oanda
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='\t'.join([
+        '%(asctime)s',
+        '%(levelname)s',
+        '%(filename)s',
+        '%(funcName)s',
+        '%(processName)s',
+        '%(process)d',
+        '%(threadName)s',
+        '%(thread)d',
+        '%(message)s']))
+logger = logging.Logger(__name__)
+
+
+class TestOanda(unittest.TestCase):
+    def setUp(self) -> None:
+        config = configparser.ConfigParser()
+        config.read('settings.ini')
+
+        self.api = Oanda(
+            account_id=config.get('OANDA', 'ACCOUNT_ID'),
+            access_token=config.get('OANDA', 'ACCESS_TOKEN'))
