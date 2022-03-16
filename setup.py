249f52723872df859dac49b63143eba5a902bba9
mniyk <my.name.is.yohei.kono@gmail.com>
2022-03-16 16:44:22 +0900

    Add: create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='oanda-api',
+    version='0.1.0',
+    author='mniyk',
+    author_email='my.name.is.yohei.kono@gmail.com',
+    description='oanda api python library',
+    long_description=long_description,
+    url='https://github.com/mniyk/oanda-api.git',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'oandapyV20'])
